a43cc8dddfab8267b6b6d9fa6bcb68c0e1b59007
Brian <bolsen@trustwave.com>
2016-10-26 21:58:51 -0500

    Removing old binaries and adding new files that were missed

---
+++ SpringModule4/src/main/java/us/brianolsen/spring/model/Robot.java
@@ -0,0 +1,26 @@
+package us.brianolsen.spring.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Robot {
+
+	private int id = 0;
+	private String speech = "hello";
+	
+	public void speak(){
+		System.out.println(id + ":" + speech);
+	}
+
+	@Autowired
+	public void setId(@Value("1138") int id) {
+		this.id = id;
+	}
+
+	@Autowired
+	public void setSpeech(@Value("Beep Boop Bop") String speech) {
+		this.speech = speech;
+	}
+}

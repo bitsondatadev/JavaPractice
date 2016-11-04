cbce47a925c343cb3f57d51e837d8d710e4a3a54
Brian <bolsen@trustwave.com>
2016-11-03 22:14:37 -0500

    Finished Module 7

---
+++ SpringModule7/src/us/brianolsen/spring/web/model/Offer.java
@@ -0,0 +1,55 @@
+package us.brianolsen.spring.web.model;
+
+public class Offer {
+	private int id;
+	private String name;
+	private String email;
+	private String text;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Offer [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", text=");
+		builder.append(text);
+		builder.append("]");
+		return builder.toString();
+	}
+}
